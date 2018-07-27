2caacf81a883da2a02e1985854eb6d3667a9d297
PATTARASAI <PATTARASAI@set.or.th>
2018-07-28 00:13:58 +0700

    add profile-component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { ProfileHeaderComponent } from './profile-header/profile-header.component';
+import { ProfileContentComponent } from './profile-content/profile-content.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProfileHeaderComponent
+    ProfileHeaderComponent,
+    ProfileContentComponent
   ],
   imports: [
     BrowserModule
