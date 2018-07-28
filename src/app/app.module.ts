6b7508d22138b43a4c82b0736aa00cab1f742a62
PATTARASAI <PATTARASAI@set.or.th>
2018-07-28 11:58:55 +0700

    add skill-content

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { ProfileHeaderComponent } from './profile-header/profile-header.component';
 import { ProfileContentComponent } from './profile-content/profile-content.component';
+import { SkillComponent } from './profile-content/skill/skill.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     ProfileHeaderComponent,
-    ProfileContentComponent
+    ProfileContentComponent,
+    SkillComponent
   ],
   imports: [
     BrowserModule
